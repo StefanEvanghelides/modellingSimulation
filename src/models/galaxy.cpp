edd8b824d90a1fba51b38bc173fec71e2608ea5c
Hidde Folkertsma <h.folkertsma.1@student.rug.nl>
2019-10-20 14:50:54 +0200

    fix bug where plotting code would crash for too many files, initialize stars in sphere instead of cube

--- src/models/galaxy.cpp
+++ src/models/galaxy.cpp
@@ -1,6 +1,7 @@
 #include "../utils/constants.h"
 #include "galaxy.h"
 #include <iostream>
+#include <cmath>
 
 
 Galaxy::Galaxy(const Coordinate& galaxyCenter, const size_t nrStars)
@@ -23,15 +24,29 @@ void Galaxy::showGalaxy() const
     std::cout << std::endl;
 }
 
+Coordinate getPoint() {
+    double x = doubleRand() - 0.5;
+    double y = doubleRand() - 0.5;
+    double z = doubleRand() - 0.5;
+
+    double mag = sqrt(x*x + y*y + z*z);
+    x /= mag; y /= mag; z /= mag;
+
+    double d = doubleRand();
+    return Coordinate {x*d, y*d, z*d};
+}
+
 const Star initializeStar(const Coordinate& galaxyCenter)
 {
     // Generate random mass
     double mass = doubleRand() * (MASS_MAX - MASS_MIN) + MASS_MIN;
 
+    Coordinate point = getPoint();
+
     // Generate random coordinates
-    const Coordinate& coord {doubleRand() * UNI_MAX_DEV + galaxyCenter.x,
-                             doubleRand() * UNI_MAX_DEV + galaxyCenter.y,
-                             doubleRand() * UNI_MAX_DEV + galaxyCenter.z};
+    const Coordinate& coord {point.x * UNI_MAX_DEV + galaxyCenter.x,
+                             point.y * UNI_MAX_DEV + galaxyCenter.y,
+                             point.y * UNI_MAX_DEV + galaxyCenter.z};
 
     // Generate random direction speed
     const Coordinate& dir {doubleRand() * DIR_MIN_DEV + DIR_MIN,
